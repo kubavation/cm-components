8a46e3b7f4461ef140e9fa0b4046daadfde852ad
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-20 10:53:00 +0100

    init of confirmation modal provider

--- projects/cm-components-lib/src/lib/modal/confirmation-modal/cm-confirmation-modal/cm-confirmation-modal.component.ts
+++ projects/cm-components-lib/src/lib/modal/confirmation-modal/cm-confirmation-modal/cm-confirmation-modal.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'lib-cm-confirmation-modal',
   templateUrl: './cm-confirmation-modal.component.html',
-  styleUrls: ['./cm-confirmation-modal.component.css']
+  styleUrls: ['./cm-confirmation-modal.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CmConfirmationModalComponent {
 
